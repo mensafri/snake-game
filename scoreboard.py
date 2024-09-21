7a70f03b3caa69a038efb72053b3120642b66e47
mensafri <safrijawad1245@gmail.com>
2024-09-21 12:56:47 +0700

    make the high score store in text

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('data.txt', mode='r') as f:
+            self.high_score = int(f.read())
         self.setpos(0, 280)
         self.color('white')
         self.hideturtle()
@@ -21,7 +22,9 @@ class Scoreboard(Turtle):
 
     def reset(self):
         if self.score > self.high_score:
-            self.high_score = self.score
+            with open('data.txt', mode='w') as f:
+                self.high_score = self.score
+                f.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
 
@@ -32,4 +35,3 @@ class Scoreboard(Turtle):
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
-
